d18630b9484106391a3fabd416e5c8be32202b9f
Freddie Sackur <github@dustyfox.uk>
2021-01-29 02:27:49 +0000

    Fake running scripts in global scope

--- Public/Import-Plugz.ps1
+++ Public/Import-Plugz.ps1
@@ -14,6 +14,11 @@ function Import-Plugz
         $PluginPath = $MyInvocation.MyCommand.Module.ModuleBase
     }
 
+
+    $ModuleProperty  = [Management.Automation.FunctionInfo].GetProperty("Module")
+    $AttributesField = [psvariable].GetField("attributes", "Nonpublic, Instance")
+
+
     foreach ($Script in $RunFirst)
     {
         if (-not [System.IO.Path]::IsPathRooted($Script))
@@ -22,11 +27,39 @@ function Import-Plugz
                 Join-Path -ChildPath $Script |
                 Where-Object {Test-Path $_} |
                 Select-Object -First 1
+
+            if (-not $Script) {continue}
         }
 
         if (Test-Path $Script)
         {
-            . $Script
+            $ScriptModule = New-Module {. $args} $Script
+            & $ScriptModule {Export-ModuleMember -Variable * -Function * -Alias *}
+
+            foreach ($Function in $ScriptModule.ExportedFunctions.Values)
+            {
+                # Use reflection to remove the DynamicModule as source
+                $ModuleProperty.SetValue($Function, $null)
+
+                $Name = $Function.Name
+                Set-Item function:global:$Name $Function
+            }
+
+            foreach ($Variable in $ScriptModule.ExportedVariables.Values)
+            {
+                $Splat = @{
+                    Name        = $Variable.Name
+                    Description = $Variable.Description
+                    Value       = $Variable.Value
+                    Visibility  = $Variable.Visibility
+                    Option      = $Variable.Options
+                    Scope       = "Global"
+                }
+                $NewVariable = Set-Variable @Splat -PassThru
+
+                # Use reflection to retroactively apply any type constraints, validation, etc
+                $AttributesField.SetValue($NewVariable, $Variable.Attributes)
+            }
         }
         else
         {
